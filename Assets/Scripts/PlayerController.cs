b1cda646c57949325081cf436c6009746f11d2cc
Vtipol <151755385+Vtipol@users.noreply.github.com>
2025-03-21 17:36:32 +0100

    PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,46 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, InputSystem_Actions.IPlayerActions
 {
-   InputActionMap actionMap;
-
-   private void OnFixedUpdate()
+   InputSystem_Actions actionMap;
+   private Vector2 MoveInput;
+   [SerializeField] public float moveSpeed = 5f;
+   public void OnAttack(InputAction.CallbackContext context) { }
+   public void OnCrouch(InputAction.CallbackContext context) { }
+   public void OnInteract(InputAction.CallbackContext context) { }
+   public void OnJump(InputAction.CallbackContext context) { }
+   public void OnLook(InputAction.CallbackContext context) { }
+   public void OnNext(InputAction.CallbackContext context) { }
+   public void OnPrevious(InputAction.CallbackContext context) { }
+   public void OnSprint(InputAction.CallbackContext context) { }
+   private void Awake()
    {
-      actionMap.
+      actionMap = new InputSystem_Actions();
+      actionMap.Player.SetCallbacks(this);
       
    }
-   
+   private void OnEnable()
+   {
+      actionMap.Player.Enable();
+   }
+   private void OnDisable()
+   {
+      actionMap.Player.Disable();
+   }
+
+   public void Update()
+   {
+      Move();
+   }
+   public void Move()
+   {
+      
+      Vector3 moveDirection = new Vector3(MoveInput.x, 0, MoveInput.y);
+      transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+   }
+   public void OnMove(InputAction.CallbackContext context)
+   {
+      MoveInput = context.ReadValue<Vector2>();
+   }
 }
