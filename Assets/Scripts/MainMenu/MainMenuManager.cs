6f351f64b5785f7a048b14f8f9aa23dfc4077073
Vtipol <151755385+Vtipol@users.noreply.github.com>
2025-04-07 12:46:59 +0200

    added script

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+
+    }
+    public void LoadPreviousGame()
+    {
+       
+    }
+    public void GoToOptions()
+    {
+
+    }
+    public void ExitGame()
+    {
+
+    }
+
+}
