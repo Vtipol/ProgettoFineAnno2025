0d9361a4dd59f28b091922a67eea5fa8d3123609
Merlin4854 <152187653+Merlin4854@users.noreply.github.com>
2025-06-26 17:26:58 +0200

    Aniamtion Fix N.70000

--- Assets/InputSystem/InputSystem_Actions.cs
+++ Assets/InputSystem/InputSystem_Actions.cs
@@ -382,7 +382,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""f2e9ba44-c423-42a7-ad56-f20975884794"",
-                    ""path"": ""<Mouse>/rightButton"",
+                    ""path"": ""<Keyboard>/leftShift"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Keyboard&Mouse"",
